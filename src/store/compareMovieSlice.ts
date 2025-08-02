70e7af2849ae9652cd4629d222f049b78279041f
Dhruv S Shah <67406262+S-Dhruv@users.noreply.github.com>
2025-08-02 13:22:48 +0530

    Comparing Movie and Tv show meta data (#27)
    * Added compare feature functionality with visualization charts
    
    * Added export to pdf and image feature

---
+++ src/store/compareMovieSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Movie, MovieDetails } from "../types/movie";
+
+interface CompareMovieState {
+  compareMovies: MovieDetails[];
+}
+
+const initialState: CompareMovieState = {
+  compareMovies: [],
+};
+
+const compareMovieSlice = createSlice({
+  name: "compare",
+  initialState,
+  reducers: {
+    addMovieToCompare: (state, action: PayloadAction<MovieDetails>) => {
+      const exists = state.compareMovies.find(
+        (movie) => movie.imdbID === action.payload.imdbID
+      );
+      if (!exists) {
+        state.compareMovies.push(action.payload);
+      }
+    },
+    removeMovieFromCompare: (state, action: PayloadAction<string>) => {
+      state.compareMovies = state.compareMovies.filter(
+        (movie) => movie.imdbID !== action.payload
+      );
+    },
+    clearCompareMovies: (state) => {
+      state.compareMovies = [];
+    },
+  },
+});
+export const { addMovieToCompare, removeMovieFromCompare, clearCompareMovies } =
+  compareMovieSlice.actions;
+
+export default compareMovieSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import movieReducer from "./movieSlice";
+import compareMovieReducer from "./compareMovieSlice";
+import { loadState, saveState } from "@/lib/localStorageUtils";
+
+const preloadedState = { compare: loadState() };
 
 export const store = configureStore({
-	reducer: {
-		movies: movieReducer,
-	},
+  reducer: {
+    movies: movieReducer,
+    compare: compareMovieReducer,
+  },
+  preloadedState,
 });
-
+store.subscribe(() => {
+  saveState(store.getState().compare);
+});
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
